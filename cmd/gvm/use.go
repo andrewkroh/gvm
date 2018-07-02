fdefe1cfa9a0d23aa27f3a0e0b111495bc4da9e0
urso <steffen.siering@elastic.co>
2018-07-02 13:22:51 +0200

    Update tests

--- cmd/gvm/use.go
+++ cmd/gvm/use.go
@@ -10,43 +10,48 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
+type useCmd struct {
+	version string
+	format  string
+}
+
 func useCommand(cmd *kingpin.CmdClause) func(*gvm.Manager) error {
-	var (
-		version string
-		format  string
-	)
-	cmd.Arg("version", "Go version to install (e.g. 1.10).").StringVar(&version)
+	ctx := &useCmd{}
+
+	cmd.Arg("version", "Go version to install (e.g. 1.10).").StringVar(&ctx.version)
 	cmd.Flag("format", "Format to use for the shell commands. Options: bash, batch, powershell").
 		Short('f').
 		Default(shellfmt.DefaultFormat()).
-		EnumVar(&format, shellfmt.BashFormat, shellfmt.BatchFormat, shellfmt.PowershellFormat)
+		EnumVar(&ctx.format, shellfmt.BashFormat, shellfmt.BatchFormat, shellfmt.PowershellFormat)
 
-	return func(manager *gvm.Manager) error {
-		if version == "" {
-			return fmt.Errorf("no version specified")
-		}
-		ver, err := gvm.ParseVersion(version)
-		if err != nil {
-			return err
-		}
-		log.Debugf("Using Go version %v", ver)
+	return ctx.Run
+}
 
-		shellFmt, err := shellfmt.New(format)
-		if err != nil {
-			return err
-		}
-
-		goroot, err := manager.Install(ver)
-		if err != nil {
-			return err
-		}
-
-		shellFmt.Set("GOROOT", goroot)
-		shellFmt.Prepend("PATH", filepath.Join(goroot, "bin"))
-		if strings.HasPrefix(version, "1.5") {
-			shellFmt.Set("GO15VENDOREXPERIMENT", "1")
-		}
-
-		return nil
+func (cmd *useCmd) Run(manager *gvm.Manager) error {
+	if cmd.version == "" {
+		return fmt.Errorf("no version specified")
 	}
+	ver, err := gvm.ParseVersion(cmd.version)
+	if err != nil {
+		return err
+	}
+	log.Debugf("Using Go version %v", ver)
+
+	shellFmt, err := shellfmt.New(cmd.format)
+	if err != nil {
+		return err
+	}
+
+	goroot, err := manager.Install(ver)
+	if err != nil {
+		return err
+	}
+
+	shellFmt.Set("GOROOT", goroot)
+	shellFmt.Prepend("PATH", filepath.Join(goroot, "bin"))
+	if strings.HasPrefix(version, "1.5") {
+		shellFmt.Set("GO15VENDOREXPERIMENT", "1")
+	}
+
+	return nil
 }
