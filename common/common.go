04ec78e2e44b95b68c6aa6c7c2bbf8e8bf76944f
Gil Raphaelli <g@raphaelli.com>
2019-02-08 17:22:19 -0500

    retry file downloads 3 times
    and set timeout on http get

--- common/common.go
+++ common/common.go
@@ -1,10 +1,12 @@
 package common
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/pkg/errors"
@@ -14,28 +16,45 @@ var log = logrus.WithField("package", "common")
 
 func DownloadFile(url, destinationDir string) (string, error) {
 	log.WithField("url", url).Debug("downloading file")
+	var name string
+	var err error
+	var retry bool
+	for a := 1; a <= 3; a++ {
+		name, err, retry = downloadFile(url, destinationDir)
+		if err != nil && retry {
+			log.WithError(err).Debug(fmt.Sprintf("Attempt %d failed", a))
+			continue
+		}
+		break
+	}
+	return name, err
+}
 
-	resp, err := http.Get(url)
+func downloadFile(url, destinationDir string) (string, error, bool) {
+	client := http.Client{
+		Timeout: time.Duration(3 * time.Minute),
+	}
+	resp, err := client.Get(url)
 	if err != nil {
-		return "", errors.Wrap(err, "http get failed")
+		return "", errors.Wrap(err, "http get failed"), true
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return "", errors.Errorf("download failed with http status %v", resp.StatusCode)
+		return "", errors.Errorf("download failed with http status %v", resp.StatusCode), resp.StatusCode != http.StatusNotFound
 	}
 
 	name := filepath.Join(destinationDir, filepath.Base(url))
 	f, err := os.Create(name)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to create output file")
+		return "", errors.Wrap(err, "failed to create output file"), false
 	}
 
 	numBytes, err := io.Copy(f, resp.Body)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to write file to disk")
+		return "", errors.Wrap(err, "failed to write file to disk"), true
 	}
 	log.WithFields(logrus.Fields{"file": name, "size_bytes": numBytes}).Debug("download complete")
 
-	return name, nil
+	return name, nil, false
 }
