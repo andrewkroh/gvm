3766033c6244fc6fd337427d77a2e2eeb7614fd9
Todd Palmer <palmer.todd@gmail.com>
2024-05-26 07:02:53 -0600

    Path separator as colon on pwsh Linux

--- cmd/gvm/internal/shellfmt/format.go
+++ cmd/gvm/internal/shellfmt/format.go
@@ -106,14 +106,21 @@ func (*batchFormatter) Append(name, val string) string {
 	return fmt.Sprintf(`set %v=%v;%v`, name, os.Getenv(name), val)
 }
 
+func getWindowsPathSeparator() string {
+	if runtime.GOOS == "windows" {
+		return ";"
+	}
+	return ":"
+}
+
 func (*powershellFormatter) Set(name, val string) string {
 	return fmt.Sprintf(`$env:%v = "%v"`, name, val)
 }
 
 func (*powershellFormatter) Prepend(name, val string) string {
-	return fmt.Sprintf(`$env:%v = "%v;$env:%v"`, name, val, name)
+	return fmt.Sprintf(`$env:%v = "%v%v$env:%v"`, name, val, getWindowsPathSeparator(), name)
 }
 
 func (*powershellFormatter) Append(name, val string) string {
-	return fmt.Sprintf(`$env:%v="$env:%v;%v"`, name, name, val)
+	return fmt.Sprintf(`$env:%v="$env:%v%v%v"`, name, name, getWindowsPathSeparator(), val)
 }
